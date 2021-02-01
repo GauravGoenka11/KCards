eda453c7afab63341f331b5f58a574e5f9ecc963
Gaurav <goenkagaurav123@gmail.com>
2021-02-01 17:03:39 +0530

    completed ProductScreen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Product from './components/Product';
-//here we have imported the data.js for dyamically display of data 
-import data from './data';
+// we have imported homescreen and productscreen
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+//we are commenting the below import bcz it is not needed in app.js we r moving it to homescreen
+// import Product from './components/Product';
+// //here we have imported the data.js for dyamically display of data 
+// import data from './data';
 function App() {
   return (
     /* by installing react-router-dom we can define multiple route for our web application  and for each route a specific screen response to it*/
@@ -27,29 +31,9 @@ function App() {
           {/* we need to create Home screen component 
           we have set exact attribute that means the path is exactly equal to the path */}
           <Route path="/" component={HomeScreen} exact></Route>
-
-
-          <div>
-            {/* we are keeping only one div with card and remove all other nine card bcause it will craete statioc website 
-        so we are taking data daynamically using map function from data.js */}
-            <div className="row center">
-              {
-                // we are using curly barces
-                // using map function to convert js product items to html elemts
-                //  for each product we convert them to the below div in the map
-                data.products.map(product => (
-                  // we create product component inside product.js and pass product object as product
-                  // so each product inside products array is goin to be pass to product component and to access we require product object
-                  // the fisrt elemt after map should have a key and key should be set the unique value
-                  // we also need to import the product component in app.js
-                  <Product key={product._id} product={product}></Product>
-
-                ))
-              }
-            </div>
-          </div>
-
         </main>
+
+
         {/* <!-- Footer --> */}
         <footer className="row center">
           All right reserved
